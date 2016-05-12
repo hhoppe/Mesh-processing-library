0f6f765fd522c65f33fd5634b68ff0385d0c4b10
Hugues Hoppe <hhoppe@gmail.com>
2016-05-12 10:56:27 -0700

    minor

--- demos/create_rendered_mechpart_image.sh
+++ demos/create_rendered_mechpart_image.sh
@@ -5,7 +5,7 @@ source bin/_initdemos.sh
 
 if [[ "${BASH_VERSINFO[5]}" == *-apple-* ]]; then
   # For some reason the "-offscreen" approach gives all-black on Mac.
-  G3dOGL data/mechpart.recon.m -st mechpart.s3d -imagename data/mechpart.image.bmp -picture $G3DARGS -geometry 800x800+100+0
+  G3dOGL data/mechpart.recon.m -st data/mechpart.s3d -imagename data/mechpart.image.bmp -picture $G3DARGS -geometry 800x800+100+0
 else
   G3dOGL data/mechpart.recon.m -st data/mechpart.s3d -offscreen data/mechpart.image.bmp -noinfo 1 $G3DARGS -geometry 800x800+100+0
 fi

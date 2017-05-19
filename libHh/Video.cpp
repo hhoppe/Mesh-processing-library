1fd39db5313f2d5a6147527ca45b86d271abbae3
Hugues Hoppe <hhoppe@gmail.com>
2017-05-19 10:06:19 -0700

    minor

--- libHh/Video.cpp
+++ libHh/Video.cpp
@@ -7,7 +7,7 @@
 #define HH_VIDEO_HAVE_MF        // unless disabled below
 #define HH_VIDEO_HAVE_FFMPEG    // always as fallback
 
-#if !defined(_MSC_VER) || defined(__clang__) || defined(HH_NO_MF)
+#if !defined(_MSC_VER) || defined(__clang__) || defined(HH_NO_WINDOWS_MEDIA_FOUNDATION)
 #undef HH_VIDEO_HAVE_MF
 #endif
 

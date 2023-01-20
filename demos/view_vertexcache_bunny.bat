fcc47e4cfaabe037a197ab5ac26b9f524fdf09d0
Hugues Hoppe <hhoppe@gmail.com>
2023-01-20 00:12:10 -0800

    remove extra DT on view_vertexcache

--- demos/view_vertexcache_bunny.bat
+++ demos/view_vertexcache_bunny.bat
@@ -8,4 +8,4 @@ echo '.'
 echo 'Drag down with the right mouse button to have a closer look.'
 echo '.'
 
-G3dOGL data/bunny.vertexcache.m -st data/bunny.s3d -key DmDTDTDC %G3DARGS%
+G3dOGL data/bunny.vertexcache.m -st data/bunny.s3d -key DmDTDC %G3DARGS%

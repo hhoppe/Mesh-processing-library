7fcd33dc55cd29c26981f7eafab34827c1557f67
Hugues Hoppe <hhoppe@gmail.com>
2024-10-26 10:28:57 -0700

    PolygonFaceSpatial -> TriangleFaceSpatial

---
+++ libHh/TriangleFaceSpatial.h
@@ -0,0 +1,86 @@
+// -*- C++ -*-  Copyright (c) Microsoft Corporation; see license.txt
+#ifndef MESH_PROCESSING_LIBHH_TRIANGLEFACESPATIAL_H_
+#define MESH_PROCESSING_LIBHH_TRIANGLEFACESPATIAL_H_
+
+#include "libHh/Facedistance.h"
+#include "libHh/GMesh.h"
+#include "libHh/Spatial.h"
+#include "libHh/Timer.h"
+
+namespace hh {
+
+struct TriangleFace {
+  Vec3<Point> triangle;
+  Face face;
+};
+
+namespace details {
+
+struct triangleface_approx_distance2 {
+  float operator()(const Point& p, Univ id) const {
+    const TriangleFace& triangleface = *Conv<const TriangleFace*>::d(id);
+    const Vec3<Point>& triangle = triangleface.triangle;
+    return square(lb_dist_point_triangle(p, triangle[0], triangle[1], triangle[2]));
+  }
+};
+
+struct triangleface_distance2 {
+  float operator()(const Point& p, Univ id) const {
+    const TriangleFace& triangleface = *Conv<const TriangleFace*>::d(id);
+    const Vec3<Point>& triangle = triangleface.triangle;
+    return dist_point_triangle2(p, triangle[0], triangle[1], triangle[2]);
+  }
+};
+
+}  // namespace details
+
+// A spatial data structure over a collection of triangles, each associated with a Mesh Face.
+class TriangleFaceSpatial
+    : public ObjectSpatial<details::triangleface_approx_distance2, details::triangleface_distance2> {
+ public:
+  explicit TriangleFaceSpatial(CArrayView<TriangleFace> trianglefaces, int gridn) : ObjectSpatial(gridn) {
+    HH_STIMER("__trianglefacespatial_build");
+    for (const TriangleFace& triangleface : trianglefaces) {
+      const Vec3<Point>& triangle = triangleface.triangle;
+      // TODO: Speed this up by avoiding use of Polygon.
+      Polygon poly{triangle}, opoly = poly;
+      const Bbox bbox{triangle};
+      const auto func_triangleface_in_bbox = [&](const Bbox<float, 3>& spatial_bbox) -> bool {
+        for_int(c, 3) if (bbox[0][c] > spatial_bbox[1][c] || bbox[1][c] < spatial_bbox[0][c]) return false;
+        const bool modified = poly.intersect_bbox(spatial_bbox);
+        const bool ret = poly.num() > 0;
+        if (modified) poly = opoly;
+        return ret;
+      };
+      ObjectSpatial::enter(Conv<const TriangleFace*>::e(&triangleface), triangle[0], func_triangleface_in_bbox);
+    }
+  }
+  // clear() inherited from ObjectSpatial
+
+  bool first_along_segment(const Point& p1, const Point& p2, const TriangleFace*& ret_ptriangleface,
+                           Point& ret_pint) const {
+    Vector vray = p2 - p1;
+    float tmin = BIGFLOAT;
+    const auto func_test_triangleface_with_ray = [&](Univ id) -> bool {
+      const TriangleFace* ptriangleface = Conv<const TriangleFace*>::d(id);
+      // TODO: Speed this up by avoiding use of Polygon.
+      const Polygon poly{ptriangleface->triangle};
+      Point pint;
+      if (!poly.intersect_segment(p1, p2, pint)) return false;
+      const float t = dot(pint - p1, vray);
+      if (t < tmin) {
+        tmin = t;
+        ret_pint = pint;
+        ret_ptriangleface = ptriangleface;
+      }
+      return true;
+    };
+    search_segment(p1, p2, func_test_triangleface_with_ray);
+    const bool found_intersection = tmin < BIGFLOAT;
+    return found_intersection;
+  }
+};
+
+}  // namespace hh
+
+#endif  // MESH_PROCESSING_LIBHH_TRIANGLEFACESPATIAL_H_
---
+++ test/TriangleFaceSpatial_test.cpp
@@ -0,0 +1,95 @@
+// -*- C++ -*-  Copyright (c) Microsoft Corporation; see license.txt
+#include "libHh/TriangleFaceSpatial.h"
+
+#include "libHh/Array.h"
+#include "libHh/Facedistance.h"
+#include "libHh/Random.h"
+using namespace hh;
+
+namespace {
+
+void test2(int gridn) {
+  const int np = 30;  // was 100
+  Array<TriangleFace> trianglefaces;
+  trianglefaces.reserve(np);
+  for_int(i, np) {
+    Vec3<Point> triangle;
+    for_int(j, 3) for_int(c, 3) triangle[j][c] = .1f + .8f * Random::G.unif();
+    trianglefaces.push({triangle, Face(intptr_t{i})});
+  }
+  TriangleFaceSpatial spatial(trianglefaces, gridn);
+  const int ns = 100;
+  for_int(j, ns) {
+    Point p;
+    for_int(c, 3) p[c] = .1f + .8f * Random::G.unif();
+    SpatialSearch<TriangleFace*> ss(&spatial, p);
+    float dis2;
+    TriangleFace& triangleface = *ss.next(&dis2);
+    int found_i = int(reinterpret_cast<intptr_t>(triangleface.face));
+    assertx(found_i == &triangleface - trianglefaces.data());
+    const Vec3<Point>& triangle1 = triangleface.triangle;
+    float rdis2 = dist_point_triangle2(p, triangle1[0], triangle1[1], triangle1[2]);
+    assertx(abs(rdis2 - dis2) < 1e-8f);
+    float mind2 = BIGFLOAT;
+    int mini = 0;
+    for_int(i, np) {
+      const Vec3<Point>& triangle2 = trianglefaces[i].triangle;
+      float d2 = dist_point_triangle2(p, triangle2[0], triangle2[1], triangle2[2]);
+      float lbd2 = square(lb_dist_point_triangle(p, triangle2[0], triangle2[1], triangle2[2]));
+      assertw(d2 >= lbd2 - 1e-12);
+      if (d2 < mind2) {
+        mind2 = d2;
+        mini = i;
+      }
+    }
+    if (found_i != mini) SHOW(j, dis2, mind2, found_i, mini);
+  }
+}
+
+}  // namespace
+
+int main() {
+  my_setenv("SHOW_STATS", "-2");
+  Timer::set_show_times(-1);
+  Face f1 = Face(intptr_t{1});
+  Face f2 = Face(intptr_t{1});
+  const Vec2<TriangleFace> trianglefaces =
+      V(TriangleFace{V(Point(.2f, .2f, .2f), Point(.2f, .8f, .8f), Point(.2f, .8f, .2f)), f1},
+        TriangleFace{V(Point(.8f, .2f, .2f), Point(.8f, .8f, .8f), Point(.8f, .8f, .2f)), f2});
+  TriangleFaceSpatial spatial(trianglefaces, 10);
+  {
+    Point p1(.1f, .5f, .3f);
+    Point p2(.9f, .5f, .3f);
+    const TriangleFace* triangleface;
+    Point pint;
+    bool ret = spatial.first_along_segment(p1, p2, triangleface, pint);
+    SHOW(ret);
+    if (ret) {
+      SHOW(pint);
+      SHOW(triangleface->triangle);
+    }
+  }
+  {
+    Point p1(.19f, .38f, .44f);
+    Point p2(.85f, .7f, .3f);
+    const TriangleFace* triangleface;
+    Point pint;
+    bool ret = spatial.first_along_segment(p1, p2, triangleface, pint);
+    SHOW(ret);
+    if (ret) {
+      SHOW(pint);
+      SHOW(triangleface->triangle);
+    }
+  }
+  {
+    SpatialSearch<TriangleFace*> ss(&spatial, Point(.4f, .3f, .3f));
+    for (;;) {
+      if (ss.done()) break;
+      float dis2;
+      TriangleFace* triangleface = ss.next(&dis2);
+      SHOW(dis2, triangleface->triangle);
+    }
+  }
+  test2(5);
+  test2(20);
+}

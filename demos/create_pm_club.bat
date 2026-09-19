4f56a9ecad978e1fb7d457cf62a503f573b59b26
Hugues Hoppe <hhoppe@gmail.com>
2026-09-18 21:07:48 -0700

    Use QEM simplification for create_pm_club

--- demos/create_pm_club.bat
+++ demos/create_pm_club.bat
@@ -4,7 +4,8 @@ setlocal
 cd "%~p0"
 call bin/_initdemos.bat
 
-echo From original mesh data/club.orig.m, creating progressive mesh results/club.pm
+echo From original mesh data/club.orig.m, creating progressive mesh results/club.pm using memoryless
+echo QEM simplification with volume preservation and normal-field preservation.
 echo (This will take a few minutes.)
 
 call bin/meshtopm.bat data/club.orig.m >results/club.pm
--- demos/create_pm_club.sh
+++ demos/create_pm_club.sh
@@ -3,10 +3,11 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"
 source bin/_initdemos.sh
 
-echo 'From original mesh data/club.orig.m, creating progressive mesh results/club.pm'
+echo 'From original mesh data/club.orig.m, creating progressive mesh results/club.pm using memoryless'
+echo 'QEM simplification with volume preservation and normal-field preservation.'
 echo '(This will take a few minutes.)'
 
-bin/meshtopm.sh data/club.orig.m >results/club.pm
+bin/meshtopm.sh data/club.orig.m -minqem >results/club.pm
 
 echo '.'
 echo 'Use view_pm_club.sh to view the resulting progressive mesh.'
